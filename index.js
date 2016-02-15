a25481a28612baffd408148fb1831adba915e43c
evenfrost <kisolov@gmail.com>
2016-02-16 01:15:10 +0300

    Add more examples and work on bundling

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-import react from 'react';
-// const react = require('react');
-// console.log('react', react);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import Counter from './components/Counter';
+import counter from './reducers';
+
+const store = createStore(counter);
+const rootEl = document.querySelector('main');
+
+function render() {
+  ReactDOM.render(
+    <Counter
+      value={store.getState()}
+      onIncrement = { function onIncrement() { return store.dispatch({ type: 'INCREMENT' }); } }
+      onDecrement = { function onDecrement() { return store.dispatch({ type: 'DECREMENT' }); } }
+    />,
+    rootEl
+  );
+}
+
+render();
+store.subscribe(render);
