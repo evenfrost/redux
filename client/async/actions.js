800b3d1713ca20c28791cd06a956247159d9dc7f
evenfrost <evenfrosty@gmail.com>
2016-05-03 19:45:59 +0300

    Work on async example

--- client/async/actions.js
+++ client/async/actions.js
@@ -1,3 +1,10 @@
 import { createAction, handleAction, handleActions } from 'redux-actions';
 
-export const SELECT_SUBREDDIT = createAction('SELECT_SUBREDDIT');
+export const selectSubreddit = createAction('SELECT_SUBREDDIT');
+export const invalidateSubreddit = createAction('INVALIDATE_SUBREDDIT');
+export const requestPosts = createAction('REQUEST_POSTS');
+export const receivePosts = createAction('RECEIVE_POSTS', (subreddit, json) => ({
+  posts: json.data.children.map(child => child.data),
+  subreddit,
+  receivedAt: Date.now(),
+}));
